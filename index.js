52040bc5adfaa6166e6b628d36983469e495fa52
kmkrish007 <muthu555krish@gmail.com>
2020-02-28 09:42:26 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './index.css';
 import * as React from 'react';
 import { SampleBase } from './sample-base';
 import { TreeViewComponent } from '@syncfusion/ej2-react-navigations';
-import { DataManager, Query, ODataV4Adaptor, UrlAdaptor } from '@syncfusion/ej2-data';
+import { DataManager, Query, ODataV4Adaptor, UrlAdaptor, Predicate } from '@syncfusion/ej2-data';
 
 const SERVICE_URI = 'https://js.syncfusion.com/demos/ejServices/api/TreeViewData/GetTreeData';
 
@@ -16,19 +16,23 @@ export class RemoteData extends SampleBase {
           adaptor: new UrlAdaptor,
           crossDomain: true,
         });
-
+ var predicate = new Predicate('hasChild', 'equal', "true" ,true);
     // Set queries to filter and fetch remote data
-    this.query = new Query().take(5);
+    this.query = new Query().where(predicate);
     this.fields = { dataSource: this.data, query: this.query, id: 'id', text: 'name', hasChildren: 'hasChild',
     child: { dataSource: this.data, query: this.query, id: 'id', parentID: 'pid', text: 'name' }
     };
+  }
+
+  nodeExpand(args){
+    debugger;
   }
 
     render() {
         return (<div className='control-pane'>
         <div className='control-section'>
         <div className='tree-control_wrapper'>
-            <TreeViewComponent fields={this.fields} />
+            <TreeViewComponent fields={this.fields} nodeExpanded={this.nodeExpand.bind(this)} />
         </div>
         </div>
       </div>);
